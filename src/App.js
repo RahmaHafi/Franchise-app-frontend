1a5b1725a9fad30765be7a72df31711347c25aec
Rahma Hafi <rahma.hafi.contact@gmail.com>
2022-04-09 13:18:29 +0100

    On page reload, detect if there is already token and user in local storage and make a client side login in that case in order to sync with redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import GlobalLoading from './components/GlobalLoading';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import {login} from './redux/actionsCreators/userActionCreators'
 
 
 
 function App() {
+
+  const dispatch = useDispatch()
+  const token = localStorage.getItem('token')
+  const user = JSON.parse(localStorage.getItem('user')) 
+  if (token && user){
+    dispatch(login(user,token))
+  }
+  
   return (
     <>
     <GlobalLoading />
